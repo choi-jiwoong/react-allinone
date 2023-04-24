414e9b8db2c885a35a74bc3a724371030a3a4a9f
choi-jiwoong <sea4choi@gmail.com>
2023-04-24 23:55:38 +0900

    state

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Extraction from './components/2-4.Props/Extraction/Extraction';
 import Composition from './components/2-4.Props/Composition';
+import FunctionalComponent from './components/2-5.State/FunctionalComponent';
 import ClassComponet from './components/2-5.State/ClassComponet';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Composition />
       <Extraction />
       <ClassComponet />
+      <FunctionalComponent />
     </div>
   );
 }
