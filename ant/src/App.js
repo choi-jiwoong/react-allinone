da618c528c1876ec2f8eb007e075bb68fb50e546
choi-jiwoong <sea4choi@gmail.com>
2023-05-16 00:05:15 +0900

    ant-design

---
+++ ant/src/App.js
@@ -0,0 +1,20 @@
+import AntDesignExample from './components/AntDesign/AntDesignExample';
+import 'antd/dist/reset.css';
+import GetStart from './components/AntDesign/GetStartExample';
+import IconExample from './components/AntDesign/IconExample';
+import TypographyExample from './components/AntDesign/TypographyExample';
+import LayoutExampe from './components/AntDesign/LayoutExample';
+import TableExample from './components/AntDesign/TableExample';
+import { Modal } from 'antd';
+import ModalExample from './components/AntDesign/ModalExample';
+
+function App() {
+  return (
+    <div>
+      <TableExample />
+      <ModalExample />
+    </div>
+  );
+}
+
+export default App;
---
+++ ant/src/components/AntDesign/AntDesignExample.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AntDesignExample() {
+  return (
+    <div>
+      <h1>AntDesignExample</h1>
+    </div>
+  )
+}
